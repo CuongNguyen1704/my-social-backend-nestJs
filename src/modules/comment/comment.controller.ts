498e46ae246fdf194add4a864cd6af4d32885a89
unknown <nguyenmanhcuong17042005@gmail.com>
2025-08-08 19:12:57 +0700

    fix create comment && getCommentByPost && getRepyly

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -1,8 +1,9 @@
-import { Body, Controller, Post, Request, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, Post, Query, Request, UseGuards } from "@nestjs/common";
 import { CommentService } from "./comment.service";
 import { JwtAuthGuard } from "../guards/jwt-auth.guard";
 import { CreateCommentDto } from "./dto/create-comment.dto";
 import { RequestWithUser } from "../auth/type/Request-with-user.interface";
+import { PaginationQueryDto } from "src/common/dto/pagination-query.dto";
 
 @Controller('comment')
 export class CommentController{
@@ -16,4 +17,20 @@ export class CommentController{
         const comment = await  this.commentService.create(commentDto,req.user.id)
         return comment
     }
+
+    @UseGuards(JwtAuthGuard)
+    @Get('list-comment-by-post/:id')
+    async listCommentByPost(@Param('id') post_id:number,@Query() paginationQuery:PaginationQueryDto ){
+        const listCommentByPost = await this.commentService.listCommentByPost(post_id,paginationQuery)
+        return listCommentByPost
+    }
+
+    @UseGuards(JwtAuthGuard)
+    @Get()
+    async ListReplyComment(@Query() paginationQueryDto:PaginationQueryDto){
+        const listReply = await this.commentService.listReply(paginationQueryDto)
+        return listReply
+    }
+
+
 }
--- src/modules/comment/comment.service.ts
+++ src/modules/comment/comment.service.ts
@@ -1,43 +1,88 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
-import { InjectRepository } from "@nestjs/typeorm";
-import { CommentEntity } from "./comment.entity";
-import { Repository } from "typeorm";
-import { CreateCommentDto } from "./dto/create-comment.dto";
-import { UserService } from "../user/user.service";
-import { PostService } from "../post/post.service";
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CommentEntity } from './comment.entity';
+import { IsNull, Not, Repository } from 'typeorm';
+import { CreateCommentDto } from './dto/create-comment.dto';
+import { UserService } from '../user/user.service';
+import { PostService } from '../post/post.service';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto';
 
 @Injectable()
-export class CommentService{
-    constructor(
-        @InjectRepository(CommentEntity)
-        private readonly commentRepository:Repository<CommentEntity>,
-        private readonly userService:UserService,
-        private readonly postService:PostService 
-    ){}
+export class CommentService {
+  constructor(
+    @InjectRepository(CommentEntity)
+    private readonly commentRepository: Repository<CommentEntity>,
+    private readonly userService: UserService,
+    private readonly postService: PostService,
+  ) {}
 
-    async create(commentDto:CreateCommentDto,user_id:number){
-        await this.postService.findById(commentDto.post_id)
-        await this.userService.findById(user_id)
+  async create(commentDto: CreateCommentDto, user_id: number) {
+    await this.postService.findById(commentDto.post_id);
+    await this.userService.findById(user_id);
 
-        let parentComment:CommentEntity | null = null
-        if(commentDto.parent_id){
-             parentComment = await this.commentRepository.findOne({
-                where:{id:commentDto.parent_id}
-            })
-            if(!parentComment){
-                throw new NotFoundException("Trả lời comment không tồn tại")
-            }
-        }
-        console.log(parentComment)
-        const createComment = this.commentRepository.create({
-            content:commentDto.content,
-            post_id:commentDto.post_id,
-            user_id:user_id,
-            parent:parentComment || undefined
-        })
-
-        const saveComment = await this.commentRepository.save(createComment)
-        
-        return saveComment 
+    let parentComment: CommentEntity | null = null;
+    if (commentDto.parent_id) {
+      parentComment = await this.commentRepository.findOne({
+        where: { id: commentDto.parent_id },
+        relations: ['post'],
+      });
+      if (!parentComment) {
+        throw new NotFoundException('Trả lời comment không tồn tại');
+      }
+      if (parentComment.post.id !== commentDto.post_id) {
+        throw new BadRequestException(
+          'Comment cha phải cùng post với comment hiện tại',
+        );
+      }
     }
+
+    const createComment = this.commentRepository.create({
+      content: commentDto.content,
+      post_id: commentDto.post_id,
+      user_id: user_id,
+      parent: parentComment || undefined,
+    });
+
+    const saveComment = await this.commentRepository.save(createComment);
+
+    return saveComment;
+  }
+
+  async listCommentByPost(
+    post_id: number,
+    paginationQuery: PaginationQueryDto,
+  ) {
+    const { page = 1, limit = 10 } = paginationQuery;
+    const [data, total] = await this.commentRepository.findAndCount({
+      where: { post_id: post_id },
+      skip: (page - 1) * limit,
+      take: limit,
+      order: { createAt: 'DESC' },
+    });
+    return {
+      data,
+      page,
+      limit,
+      total,
+      lastPage: Math.ceil(total / limit),
+    };
+  }
+
+  async listReply(paginationQuery: PaginationQueryDto) {
+    const { page = 1, limit = 25 } = paginationQuery;
+    const [data, total] = await this.commentRepository.findAndCount({
+      where: { parent: Not(IsNull()) },
+      skip: (page - 1) * limit,
+      take: limit,
+      order: { createAt: 'DESC' },
+    });
+
+    return {
+      data,
+      page,
+      limit,
+      total,
+      lastPage: Math.ceil(total / limit),
+    };
+  }
 }
